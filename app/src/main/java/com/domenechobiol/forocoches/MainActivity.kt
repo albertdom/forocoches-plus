4f9184fd4c8a26087f33689d08031d91ab1f987e
albertd987 <domenechobiol@gmail.com>
2026-05-24 11:32:18 +0200

    feat: basic WebView activity loading Forocoches

---
+++ app/src/main/java/com/domenechobiol/forocoches/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.domenechobiol.forocoches
+
+import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        configureWebView()
+        webView.loadUrl("https://forocoches.com/foro/")
+    }
+
+    private fun configureWebView() {
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            setSupportZoom(true)
+            builtInZoomControls = true
+            displayZoomControls = false
+            userAgentString = "Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+        }
+        CookieManager.getInstance().apply {
+            setAcceptCookie(true)
+            setAcceptThirdPartyCookies(webView, true)
+        }
+    }
+
+    @Deprecated("Needed for API < 33")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+}
